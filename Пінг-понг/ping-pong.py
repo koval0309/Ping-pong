d6973488ccced34d4abf4ac2ad4ba4dc0af463cf
Коваль <157277961+koval0309@users.noreply.github.com>
2024-01-28 12:50:45 +0200

    Add files via upload

--- Пінг-понг/ping-pong.py
+++ Пінг-понг/ping-pong.py
@@ -26,14 +26,30 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update(self):
+    def right(self):
         keys = key.get_pressed()
-        if keys[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if keys [K_d] and self.rect.x < win_width - 80:
-            self.rect.x += self.speed
-            
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y += self.rect.y
+        if keys [K_d] and self.rect.x < win_height - 80:
+            self.rect.y -= self.rect.y
+
+    def left(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y += self.rect.y
+        if keys [K_s] and self.rect.y < win_height - 80:
+            self.rect.y -= self.rect.y
+
 display.set_caption("Пінг-Понг")
 window = display.set_mode((win_width, win_height))
+background = (75, 156, 98)
+window.fill(background)
 
+finish = False
 
+run = True
+while run:
+
+    display.update()
+
+    time.delay(50)
