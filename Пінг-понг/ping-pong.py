7aa0fbb6999dbd4556cb9d24c03343e231107060
Коваль <157277961+koval0309@users.noreply.github.com>
2024-02-11 12:27:34 +0200

    Add files via upload

--- Пінг-понг/ping-pong.py
+++ Пінг-понг/ping-pong.py
@@ -6,20 +6,19 @@ win_height = 500
 speed_x = 3
 speed_y = 3
 
-img_truba1 = "Truba1.png"
-img_truba2 = "Truba2.png"
-img_ball = "boll.png"
-
+font.init()
+font1 = font.Font(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, size_x, sixe_y, player_speed):
-        sprite.Sprite.__init__(self)
-        self.image = transform.scale(image.load(player_image), (size_x, sixe_y))
+    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (wight, height)) 
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-
 
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
@@ -40,40 +39,53 @@ class Player2(GameSprite):
             self.rect.y += self.speed
 
 
-display.set_caption("Пінг-Понг")
+back = (200, 255, 255)  
+win_width = 600
+win_height = 500
 window = display.set_mode((win_width, win_height))
-background = (75, 156, 98)
-window.fill(background)
+window.fill(back)
 
-raketka_left = Player1(img_truba1, 50, 350, 70, 100, 5 )
-raketka_right = Player2(img_truba1, 570, 50, 70, 100, 5 )
-ball = GameSprite(img_ball, 200, 200, 50, 50, 20)
+racket1 = Player1('Truba1.png', 30, 200, 4, 50, 150) 
+racket2 = Player2('Truba1.png', 520, 200, 4, 50, 150)
+ball = GameSprite('ball.png', 200, 200, 4, 50, 50)
 
+game = True
 finish = False
 clock = time.Clock()
 FPS = 60
-run = True
 
-while run:
+while game:
     for e in event.get():
         if e.type == QUIT:
-            run = False
-
-    if not finish:
-        window.fill(background)
-        raketka_left.update()
-        raketka_right.update()
-        ball.update()
-        raketka_right.reset()
-        ball.reset()
-        raketka_left.reset()
+            game = False
+  
+    if finish != True:
+        window.fill(back)
+        racket1.update()
+        racket2.update()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
-
-    if ball.rect.y > win_height-50 or ball.rect.y < 0:
-        speed_y *= -1
-    if sprite.collide_rect(img_truba1, ball) or sprite.collide_rect(img_truba2, ball):
-        speed_x *= -1
-        display.update()
-
+    
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
+        
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+    
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+            game_over = True
+    
+        if ball.rect.x > win_width:
+            finish = True
+            window.blit(lose2, (200, 200))
+            game_over = True
+    
+        racket1.reset()
+        racket2.reset()
+        ball.reset()
+    
+    display.update()
     clock.tick(FPS)
